31379a08469fb401904f955302d292814afbbad8
LaxWasHere <public@LaxWasHere.com>
2013-08-29 14:52:49 -0700

    Actually make it for public use.

--- src/net/lazlecraft/donotdrop/DoNotDrop.java
+++ src/net/lazlecraft/donotdrop/DoNotDrop.java
@@ -1,5 +1,7 @@
 package net.lazlecraft.donotdrop;
 
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,30 +13,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class DoNotDrop extends JavaPlugin implements Listener {
 	
 	public String prefix;
-	public String skyworld = "Skyblock";
+	public String onDeath;
+	public String onDrop;
+	public List<String> nodropworld;
 	
 	public void onEnable() {
 		this.getConfig().options().copyDefaults(true);
 		this.saveConfig();
 		getServer().getPluginManager().registerEvents(this, this);
 		prefix = getConfig().getString("prefix");
+		onDeath = getConfig().getString("MessageOnDeath");
+		onDrop = getConfig().getString("MessageOnDrop");
+		nodropworld = getConfig().getStringList("Worlds");
 	}
 	
 	@EventHandler
 	public void onDrop(PlayerDropItemEvent ev) {
 		Player p = ev.getPlayer();
-				if (ev.getPlayer().getLocation().getWorld().getName().equals(skyworld)) {
+				if (nodropworld.contains(p.getLocation().getWorld().getName())) {
 					ev.setCancelled(true);
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix) + ChatColor.RED + " You may not drop items in this world!");
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix) + ChatColor.translateAlternateColorCodes('&', onDrop));
 		}
 	}
 	
 	@EventHandler
 	public void onDeath(PlayerDeathEvent ev) {
 		Player p = ev.getEntity();
-		if (p.getLocation().getWorld().getName().equals(skyworld)) {
+		if (nodropworld.contains(p.getLocation().getWorld().getName())) {
 			ev.getDrops().clear();
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix) + ChatColor.RED + "You died on the spawn world and lost all your items!");
+			p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix) + ChatColor.translateAlternateColorCodes('&', onDeath));
 		}
 	}
 }
